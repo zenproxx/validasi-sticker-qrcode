0838e1cb19c4116d4b7741f009c6979e45ef7acc
Muhammad Fakhri <fakhri.warnet@live.com>
2019-09-18 13:28:42 +0700

    update

--- qrvalidasi.sql
+++ qrvalidasi.sql
@@ -1,26 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Inang: 127.0.0.1
--- Waktu pembuatan: 06 Sep 2019 pada 19.27
--- Versi Server: 5.5.32
--- Versi PHP: 5.4.19
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 18 Sep 2019 pada 08.28
+-- Versi server: 10.1.39-MariaDB
+-- Versi PHP: 7.1.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Basis data: `qrvalidasi`
+-- Database: `qrvalidasi`
 --
-CREATE DATABASE IF NOT EXISTS `qrvalidasi` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `qrvalidasi`;
 
 -- --------------------------------------------------------
 
@@ -28,13 +28,12 @@ USE `qrvalidasi`;
 -- Struktur dari tabel `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
-  `idadmin` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `admin` (
+  `idadmin` int(5) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `namalengkap` varchar(40) NOT NULL,
-  PRIMARY KEY (`idadmin`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+  `namalengkap` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data untuk tabel `admin`
@@ -50,24 +49,57 @@ INSERT INTO `admin` (`idadmin`, `username`, `password`, `namalengkap`) VALUES
 -- Struktur dari tabel `mahasiswa`
 --
 
-CREATE TABLE IF NOT EXISTS `mahasiswa` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `mahasiswa` (
+  `id` int(10) NOT NULL,
   `npm` varchar(10) DEFAULT NULL,
   `nama_mhs` varchar(40) DEFAULT NULL,
   `prodi` varchar(40) DEFAULT NULL,
   `tgl_lulus` date DEFAULT NULL,
   `no_ijazah` varchar(40) DEFAULT NULL,
   `ipk` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=34 ;
+  `ket_berlaku` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data untuk tabel `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`id`, `npm`, `nama_mhs`, `prodi`, `tgl_lulus`, `no_ijazah`, `ipk`) VALUES
-(32, '0582 5395 ', '99595 99595 59959', 'Afdeling I', '2019-07-01', 'BK 5995 RG', 'ADOLINA'),
-(33, '9876 0987', '9887 - 0912 -8761 -0982', 'Afdeling X', '2019-09-13', 'BK 2399 IUL', 'PASIR MANDOGE');
+INSERT INTO `mahasiswa` (`id`, `npm`, `nama_mhs`, `prodi`, `tgl_lulus`, `no_ijazah`, `ipk`, `ket_berlaku`) VALUES
+(32, '0582 5395 ', '99595 99595 59959', 'Afdeling I', '2019-07-01', 'BK 5995 RG', 'ADOLINA', 'Berlaku Sampai Dengan Juli 2019'),
+(33, '9876 0987', '9887 - 0912 -8761 -0982', 'Afdeling X', '2019-09-13', 'BK 2399 IUL', 'PASIR MANDOGE', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`idadmin`);
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `idadmin` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
